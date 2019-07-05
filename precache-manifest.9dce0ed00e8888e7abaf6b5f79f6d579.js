58fa9742b4bb103607988772c725f83767479e24
Vitaly <polevit@yandex.ru>
2019-07-05 12:21:50 +0300

    Fix form banner

--- precache-manifest.9dce0ed00e8888e7abaf6b5f79f6d579.js
+++ precache-manifest.9dce0ed00e8888e7abaf6b5f79f6d579.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "136bff5f8ac95035ffdfae2247f81d3d",
+    "revision": "4c55849dfb9c2540e590f8cbdc3ece47",
     "url": "/index.html"
   },
   {
-    "revision": "03c85eb17f598aaea5ce",
-    "url": "/static/css/main.c5694c0a.chunk.css"
+    "revision": "2d461c7f73f08d44d6f8",
+    "url": "/static/css/main.37a18a85.chunk.css"
   },
   {
     "revision": "e40b572e4e54d1692dad",
     "url": "/static/js/2.31480381.chunk.js"
   },
   {
-    "revision": "03c85eb17f598aaea5ce",
+    "revision": "2d461c7f73f08d44d6f8",
     "url": "/static/js/main.72978aa5.chunk.js"
   },
   {
